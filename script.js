238ac5eb9ec89a98e09d24b1397f9f8406bf7710
Erick Silva <donizttierick@gmail.com>
2025-10-19 14:27:40 -0300

    Pronto

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-setInterva(function(){
+    indice = 0
+    setInterval(function(){
+    let mensa = document.getElementById('txt')
+    
+    let frases = [ 
+        "Sinta o aroma da felicidade",
+        "Cada pão, uma história",
+        "O sabor que transforma o dia",
+        "Comece o dia com alegria",
+        "Delícias feitas com amor",
+        "Tradição que se renova a cada fornada",
+        "Mais que pão, momentos",
+        "O melhor da manhã está aqui",
+        "Sabor que desperta sorrisos",
+        "A felicidade tem cheiro de pão quentinho",
+        "Cada fornada é feita com carinho",
+        "Amor e pão em cada fatia",
+        "O dia começa melhor com pão quentinho",
+        "Sabores que encantam a todos",
+        "Venha experimentar a alegria em forma de pão"
+    ]
+    // InnerText e indice
+    mensa.innerText = (frases[indice])
+    indice++
+
+    // If
+    if (indice >= frases.length) {
+        indice = 0
+    }
     
 }, 1000)
