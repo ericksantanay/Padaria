9d37230081b2d6d348ec33c8eda93753258dabc0
Erick Silva <donizttierick@gmail.com>
2025-10-21 15:04:00 -0300

    .

--- script.js
+++ script.js
@@ -61,10 +61,10 @@ function Compra() {
   
 
 // MENU HAMBURGUER 
-    function ClickMenu() {
-        if (menu_mobile.style.display == 'block') {
-            menu_mobile.style.display = 'none'
-        }else {
-            menu_mobile.style.display = 'block'
+      function ClickMenu(){
+            if (menu_mobile.style.display == 'block'){
+                menu_mobile.style.display = 'none'
+            }else{
+                menu_mobile.style.display = 'block'
+            }
         }
-    }
